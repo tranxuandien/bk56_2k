57b97fdf4dae1f67c4a503e4b9c5e29e1f13a6e5
anhtuyen1512 <anhtuyen_302@yahoo.com>
2015-03-12 09:43:04 +0000

    Questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,48 @@
+class QuestionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    @questions = Question.all
+    respond_with(@questions)
+  end
+
+  def show
+    respond_with(@question)
+  end
+
+  def new
+    @question = Question.new
+    respond_with(@question)
+  end
+
+  def edit
+  end
+
+  def create
+    @question = Question.new(question_params.merge!({user: current_user})) 
+    @question.save
+    respond_with(@question)
+  end
+
+  def update
+    @question.update(question_params)
+    respond_with(@question)
+  end
+
+  def destroy
+    @question.destroy
+    respond_with(@question)
+  end
+
+  private
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    def question_params
+      params.require(:question).permit(:title, :content, :user_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :questions
+
+  devise_for :users
+  root 'static_pages#home'
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
